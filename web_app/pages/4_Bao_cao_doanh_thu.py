770776210f52820a09d6d8a1e77d8ebba3c1e707
NGO THE DAT <188000162+Ngo-The-Dat@users.noreply.github.com>
2025-12-15 20:19:44 +0700

    thêm lịch sử thú cưng

--- web_app/pages/4_Bao_cao_doanh_thu.py
+++ web_app/pages/4_Bao_cao_doanh_thu.py
@@ -5,7 +5,7 @@ from services.baocao_service import bao_cao_doanh_thu_theo_nam
 
 st.header("Báo cáo doanh thu theo năm")
 
-nam = st.number_input("Năm", min_value=2000, step=1, format="%d")
+nam = st.number_input("Năm", step=1)
 
 if nam:
 	df = bao_cao_doanh_thu_theo_nam(int(nam))
@@ -15,9 +15,9 @@ if nam:
 	else:
 		st.dataframe(df, hide_index=True)
 
-		chart_data = df.copy()
-		chart_data["Tháng"] = chart_data["THANG"].apply(lambda x: f"Tháng {int(x)}")
-		st.bar_chart(chart_data.set_index("Tháng")["DOANHTHU"], height=400)
+		# chart_data = df.copy()
+		# chart_data["Tháng"] = chart_data["THANG"].apply(lambda x: f"Tháng {int(x)}")
+		# st.bar_chart(chart_data.set_index("Tháng")["DOANHTHU"], height=400)
 else:
 	st.info("Nhập năm để xem báo cáo.")
 # dynamic list triệu chứng, thuốc...
