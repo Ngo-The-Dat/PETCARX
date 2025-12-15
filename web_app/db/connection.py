05075434cc0a34ff25bd348e7fb3b0580f48178f
NGO THE DAT <188000162+Ngo-The-Dat@users.noreply.github.com>
2025-12-15 21:44:30 +0700

    fix connection

--- web_app/db/connection.py
+++ web_app/db/connection.py
@@ -3,7 +3,7 @@ import pyodbc
 def get_connection():
     return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=localhost\\SQLEXPRESS;"
+        "SERVER=localhost;"
         "DATABASE=PETCARX;"
         "Trusted_Connection=yes;"
     )
