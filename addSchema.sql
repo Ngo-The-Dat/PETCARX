206008a4f18f876063313237e85112483e80f406
To My <tomy02032005@gmail.com>
2025-12-19 21:31:13 +0700

    Add xem lich su mua hang cua khach hang

--- addSchema.sql
+++ addSchema.sql
@@ -568,3 +568,38 @@ BEGIN
     WHERE MATC = @MATC
 END
 GO
+
+----Xem lịch sử mua hàng của khách hàng----
+CREATE PROCEDURE sp_LichSuMuaHang
+    @MATK INT
+AS
+BEGIN
+    SELECT 
+        hd.MAHD,
+        hd.NGAYLAP,
+        hd.TONGTIEN,
+        hd.HINHTHUCTHANHTOAN,
+        cn.TEN AS TENCHINHANH,
+        cn.DIACHI AS DIACHICHINHANH,
+        sp.TEN AS TENSANPHAM,
+        ctsp.SOLUONG AS SOLUONGSP,
+        ctsp.DONGIAHIENTAI AS DONGIASP,
+        ctsp.THANHTIEN,
+        dv.TENDV AS TENDICHVU,
+        ctdv.DONGIAHIENTAI AS DONGIADV,
+        tc.TEN AS TENTHUCUNG
+    FROM HOADON hd
+    LEFT JOIN CHINHANH cn ON hd.MACN = cn.MACN
+    LEFT JOIN CTHDSANPHAM ctsp ON hd.MAHD = ctsp.MAHD
+    LEFT JOIN SANPHAM sp ON ctsp.MASP = sp.MASP
+    LEFT JOIN CTHDDV ctdv ON hd.MAHD = ctdv.MAHD
+    LEFT JOIN DICHVU dv ON ctdv.MADV = dv.MADV
+    LEFT JOIN THUCUNG tc ON ctdv.MATC = tc.MATC
+    WHERE hd.MATK = @MATK
+    ORDER BY hd.NGAYLAP DESC, hd.MAHD DESC
+END
+GO
+
+SELECT *
+FROM NHANSU
+WHERE CHUCVU = N'Bác sĩ thú y'
