801fb94505720f0d4a357450055812c64a1b09f4
Tin Nguyen <nguyentrongtin2807@gmail.com>
2025-12-20 08:53:29 +0700

    bỏ chức năng tra cứu hồ sơ vì đã gộp nó vào lịch sử khám

--- web_app/pages/doctor.py
+++ web_app/pages/doctor.py
@@ -3,9 +3,6 @@ import streamlit as st
 st.title("Doctor")
 st.write("Chọn chức năng:")
 
-if st.button("Tra cứu hồ sơ khám của thú cưng"):
-    st.switch_page("pages/customer_search_product.py") # đổi đường dẫn
-
 if st.button("Tra cứu lịch sử khám của thú cưng"):
     st.switch_page("pages/doctor_pet_medical_history.py")
 
