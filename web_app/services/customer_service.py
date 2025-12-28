048043183e15bea7ffc8ca440a9e6134ce13d437
NGO THE DAT <188000162+Ngo-The-Dat@users.noreply.github.com>
2025-12-28 21:01:37 +0700

    Remove unused index creation statements and delete baocao_service.py; refactor customer_service.py by removing get_pet_by_id function.

--- web_app/services/customer_service.py
+++ web_app/services/customer_service.py
@@ -2,20 +2,6 @@ import pandas as pd
 
 from db.connection import get_connection
 
-# Tra cứu thú cưng (Tìm kiếm thú cưng)
-def get_pet_by_id(matc: int) -> pd.DataFrame:
-    """
-    Gọi sp_TraCuuThuCung
-    Trả về thông tin thú cưng theo MATC
-    """
-    conn = get_connection()
-
-    query = "EXEC sp_TraCuuThuCung ?"
-    df = pd.read_sql(query, conn, params=[matc])
-
-    conn.close()
-    return df
-
 
 def get_all_customers():
     """Lấy danh sách tất cả khách hàng (SDT, HOTEN)."""
